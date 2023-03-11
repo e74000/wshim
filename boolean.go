e20e82a06b0537745f3e672dc862e3b13e223f6b
e74000 <olimcinnes@protonmail.com>
2023-03-11 21:47:16 +0000

    Add missing ID

--- boolean.go
+++ boolean.go
@@ -7,7 +7,6 @@ import (
 type Toggle struct {
 	Name, Key string
 	Val       *bool
-	Init      bool
 }
 
 func (t *Toggle) Build() (label, key, sType string, elems []js.Value) {
@@ -15,7 +14,8 @@ func (t *Toggle) Build() (label, key, sType string, elems []js.Value) {
 
 	toggle := document.Call("createElement", "input")
 	toggle.Call("setAttribute", "type", "checkbox")
-	toggle.Call("setAttribute", "checked", t.Init)
+	toggle.Call("setAttribute", "checked", *t.Val)
+	toggle.Call("setAttribute", "id", t.Key)
 	toggle.Call("setAttribute", "class", "optionToggle")
 
 	update[t.Key] = func(v any) {
